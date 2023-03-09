23688d795e08a09f035db7ff41c9bd9a0933df27
J8051 <nathaniel.jessica@yahoo.ca>
2023-03-08 19:23:32 -0500

    twillio working with env vars

--- twillio/sendsms.js
+++ twillio/sendsms.js
@@ -4,8 +4,6 @@ require('dotenv').config();
 const accountSid = process.env.TWILIO_ACCOUNT_SID // Your Account SID from www.twilio.com/console
 const authToken = process.env.TWILIO_AUTH_TOKEN; // Your Auth Token from www.twilio.com/console
 
-// console.log("account", accountSid);
-// console.log("auth token",authToken);
 const client = require('twilio')(accountSid, authToken);
 
 
